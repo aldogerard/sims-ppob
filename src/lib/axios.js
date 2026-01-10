f079570a5b3a514967cae2f1595a980cb5c37fa2
aldogerard <aldogerard12@gmail.com>
2026-01-10 13:39:03 +0700

    refactor(axios): remove withCredentials from axios instance configuration

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const axiosInstance = (baseUrl) => {
     const instance = axios.create({
         baseURL: baseUrl,
-        withCredentials: true,
     });
 
     instance.interceptors.request.use((config) => {
